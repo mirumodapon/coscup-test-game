e84fbdfff68fd02dfe0af5308870b0f1aeef6fc6
Hsinyu Tsai <104778754+hsinyu2527@users.noreply.github.com>
2025-06-29 00:10:27 +0800

    feat(ui): add button icons, improve styling, andupdate left button to show sponsor list

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,23 @@
 <script setup lang="ts">
 import PhaserGame from './PhaserGame.vue'
+import './style.css'
 </script>
 
 <template>
   <PhaserGame ref="phaserRef" />
   <div class="bottom-bar">
-    <button class="button">成就</button>
-    <button class="button">分享</button>
-    <button class="button">個人檔案</button>
+    <button class="button">
+      <img src="./icons/sponsor.png" alt="贊助商列表" />
+      <span>贊助商列表</span>
+    </button>
+    <button class="button button-game">
+      <img src="./icons/game.png" alt="遊戲畫面" />
+      <span>遊戲畫面</span>
+    </button>
+    <button class="button">
+      <img src="./icons/user.png" alt="我的資料" />
+      <span>我的資料</span>
+    </button>
   </div>
 </template>
 
@@ -16,12 +26,33 @@ import PhaserGame from './PhaserGame.vue'
   position: absolute;
   display: flex;
   bottom: 0;
-  height: 40px;
+  height: 70px;
   width: 100%;
+  justify-content: space-around;
+  align-items: center;
+  background-color: #e0ecff;
   z-index: 10;
+  box-shadow: 0 -2px 8px rgba(0, 0, 0, 0.2);
 }
+
 .button {
   flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 5px;
+  background: transparent;
+  border: none;
+  outline: none;
+  font-size: 14px;
+  font-weight: 600;
+  color: #333;
   margin: 0;
+  padding: 0;
+}
+
+.button img {
+  width: 32px;
+  height: 32px;
 }
 </style>
--- src/game/GameData.ts
+++ src/game/GameData.ts
@@ -6,7 +6,7 @@ export const GameData = {
   screenWidth: 0,
   screenHeight: 0,
   hexSize: 0,
-  bottomBarHeight: 40,
+  bottomBarHeight: 70,
   popupOpen: false,
   get hexWidth() {
     return this.hexSize * 2
